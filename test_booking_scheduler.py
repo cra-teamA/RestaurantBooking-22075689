59f0e87b5f15809435f1e41fa8a57b523cd735fe
hwieun <hwieunlee98@gmail.com>
2025-08-01 13:21:35 +0900

    [refactor] Step12

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from os import supports_effective_ids
 
 import pytest
 
@@ -13,6 +14,14 @@ CUSTOMER_WITH_MAIL = Customer("Fake name", "010-1234-5678", "test@test.com")
 
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
+
+class TestableBookingScheduler(BookingScheduler):
+    def __init__(self, capacity_per_hour, date_time: str):
+        super().__init__(capacity_per_hour)
+        self._date_time = date_time
+
+    def get_now(self):
+        return datetime.strptime(self._date_time, "%Y/%m/%d %H:%M")
 
 @pytest.fixture
 def booking_scheduler():
@@ -108,7 +117,22 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
     assert mail_mock.send_mail_count == 1
 
 def test_현재날짜가_일요일인_경우_예약불가_예외처리():
-    pass
+    # arrange
+    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2021/03/28 17:00")
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
+    # act and assert
+    with pytest.raises(ValueError):
+        booking_scheduler.add_schedule(schedule)
+
 
 def test_현재날짜가_일요일이_아닌경우_예약가능():
-    pass
+    # arrange
+    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2024/06/03 17:00")
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
+    # act
+    booking_scheduler.add_schedule(schedule)
+
+    # assert
+    assert booking_scheduler.has_schedule(schedule)
